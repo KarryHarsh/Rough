d195c25b1454797797428884370ddedc5d1056cb
Karry Harsh <49169924+KarryHarsh@users.noreply.github.com>
2023-09-08 13:41:24 +0530

    Update Multi threading.py

--- Multi threading.py
+++ Multi threading.py
@@ -1,35 +1,51 @@
-import threading
+def merge_nested_dicts(a, b):
+    c = {}
+    
+    # Merge dictionary 'a' into 'c'
+    for key_a, value_a in a.items():
+        if key_a in c:
+            # Merge subvalue and featurename
+            for subkey, subvalue in value_a.items():
+                if subkey in c[key_a]:
+                    c[key_a][subkey].extend(subvalue)
+                else:
+                    c[key_a][subkey] = subvalue
+        else:
+            c[key_a] = value_a
+    
+    # Merge dictionary 'b' into 'c'
+    for key_b, value_b in b.items():
+        if key_b in c:
+            # Merge subvalue and featurename
+            for subkey, subvalue in value_b.items():
+                if subkey in c[key_b]:
+                    c[key_b][subkey].extend(subvalue)
+                else:
+                    c[key_b][subkey] = subvalue
+        else:
+            c[key_b] = value_b
+    
+    return c
 
-# Define the function you want to execute with arguments
-def my_function(arg):
-    print(f"Thread {threading.current_thread().name} with argument: {arg}")
+# Example dictionaries a and b
+a = {
+    'subvalue1': {
+        'featurename1': [
+            {'feature1': 'featurevalue1'},
+            {'feature2': 'featurevalue2'}
+        ]
+    }
+}
 
-# Define a function to create and start threads
-def create_threads():
-    arguments = [1, 2, 3, 4, 5]  # List of arguments to pass to the function
+b = {
+    'subvalue2': {
+        'featurename2': [
+            {'featurea': 'featurevaluea'},
+            {'featureb': 'featurevalueb'}
+        ]
+    }
+}
 
-    # Create and start a thread for each argument
-    threads = []
-    for arg in arguments:
-        thread = threading.Thread(target=my_function, args=(arg,))
-        threads.append(thread)
-        thread.start()
-
-    # Wait for all threads to finish
-    for thread in threads:
-        thread.join()
-
-if __name__ == "__main__":
-    num_threads = 3  # Number of threads you want to run concurrently
-
-    # Create and start the specified number of threads
-    for i in range(num_threads):
-        thread = threading.Thread(target=create_threads)
-        thread.start()
-
-    # Wait for all threads that create and start threads to finish
-    for i in range(num_threads):
-        thread.join()
-
-    print("All threads have finished.")
-  
+# Merge dictionaries a and b into c
+c = merge_nested_dicts(a, b)
+print(c)
