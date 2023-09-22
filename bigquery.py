c7e9169ebb02bb5fc9fca893225a13a361043677
Karry Harsh <49169924+KarryHarsh@users.noreply.github.com>
2023-09-22 11:03:39 +0530

    Update bigquery.py

--- bigquery.py
+++ bigquery.py
@@ -1,36 +1,8 @@
-from google.cloud import storage
-import os
+def list_sql_files_in_directory(directory_path):
+    sql_files = []
 
-def copy_files_from_gcs_bucket(bucket_name, source_directory, destination_directory):
-    # Initialize the Google Cloud Storage client
-    client = storage.Client()
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".sql"):
+            sql_files.append(filename)
 
-    # Get the GCS bucket
-    bucket = client.get_bucket(bucket_name)
-
-    # List all objects in the source directory
-    blobs = bucket.list_blobs(prefix=source_directory)
-
-    # Ensure the destination directory exists locally
-    os.makedirs(destination_directory, exist_ok=True)
-
-    # Copy each object from GCS to the local directory
-    for blob in blobs:
-        source_blob_name = blob.name
-        destination_file_name = os.path.join(destination_directory, os.path.basename(source_blob_name))
-
-        # Download the file from GCS to the local directory
-        blob.download_to_filename(destination_file_name)
-
-if __name__ == "__main__":
-    # Replace with your GCS bucket name
-    bucket_name = "your-bucket-name"
-
-    # Replace with the source directory in the bucket (leave empty for the root)
-    source_directory = "your/source/directory"
-
-    # Replace with the local destination directory
-    destination_directory = "path/to/local/destination"
-
-    copy_files_from_gcs_bucket(bucket_name, source_directory, destination_directory)
-    print("Files copied from GCS bucket to the local directory.")
+    return sql_files
