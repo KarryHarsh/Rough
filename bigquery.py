e1c4c0441da6b4232a22a7c976774dc56cd279ef
Karry Harsh <49169924+KarryHarsh@users.noreply.github.com>
2023-09-22 10:05:42 +0530

    Update bigquery.py

--- bigquery.py
+++ bigquery.py
@@ -1,30 +1,36 @@
-import pathlib
-import re
+from google.cloud import storage
+import os
 
-def replace_strings_in_sql_files(directory_path, source_str, destination_str):
-    # Create a Path object for the directory
-    directory = pathlib.Path(directory_path)
+def copy_files_from_gcs_bucket(bucket_name, source_directory, destination_directory):
+    # Initialize the Google Cloud Storage client
+    client = storage.Client()
 
-    # Use a list comprehension to find all .sql files in the directory
-    sql_files = [file for file in directory.glob("*.sql")]
+    # Get the GCS bucket
+    bucket = client.get_bucket(bucket_name)
 
-    # Iterate through each SQL file and perform the replacement
-    for sql_file in sql_files:
-        with open(sql_file, "r") as file:
-            sql_content = file.read()
+    # List all objects in the source directory
+    blobs = bucket.list_blobs(prefix=source_directory)
 
-        # Perform string replacements using regular expressions
-        sql_content = re.sub(r'fs_analytics_tbls', destination_str, sql_content)
-        sql_content = re.sub(r'ds_fraud_dmz', source_str, sql_content)
+    # Ensure the destination directory exists locally
+    os.makedirs(destination_directory, exist_ok=True)
 
-        # Write the modified content back to the file
-        with open(sql_file, "w") as file:
-            file.write(sql_content)
+    # Copy each object from GCS to the local directory
+    for blob in blobs:
+        source_blob_name = blob.name
+        destination_file_name = os.path.join(destination_directory, os.path.basename(source_blob_name))
+
+        # Download the file from GCS to the local directory
+        blob.download_to_filename(destination_file_name)
 
 if __name__ == "__main__":
-    directory_path = "path/to/your/sql/files"  # Replace with your directory path
-    source_str = "{{source_dataset}}"
-    destination_str = "{{destination_dataset}}"
-    
-    replace_strings_in_sql_files(directory_path, source_str, destination_str)
-    print("String replacements completed.")
+    # Replace with your GCS bucket name
+    bucket_name = "your-bucket-name"
+
+    # Replace with the source directory in the bucket (leave empty for the root)
+    source_directory = "your/source/directory"
+
+    # Replace with the local destination directory
+    destination_directory = "path/to/local/destination"
+
+    copy_files_from_gcs_bucket(bucket_name, source_directory, destination_directory)
+    print("Files copied from GCS bucket to the local directory.")
